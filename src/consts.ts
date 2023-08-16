f94ce0fd9fba45960ffd111337388f3b1096b776
Canaris <mrgeek484@gmail.com>
2023-08-16 09:23:11 +0800

    g

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'CCA Demo';
+export const SITE_DESCRIPTION = 'yes';
+export const PFP_URL = 'https://avatars.githubusercontent.com/u/68373699?v=4';
+export const GITHUB = 'https://github.com/withastro/astro';
+export const MASTODON = 'https://m.webtoo.ls/@astro';
+export const TWITTER = 'https://twitter.com/astrodotbuild';
+export const NAME = 'Astro';
